5af88adb4c809d0499bbd191e72d21e2a4461485
Patrick Lawrence <patrickjameslawrence@icloud.com>
2023-11-01 00:05:16 -0400

    Added templates folder to be copied into build folder on build

--- src/components/assets/DefaultAvatar.wireframe.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import { StaticImage } from "gatsby-plugin-image"
-
-export default function WideLogo() {
-  return <StaticImage className="h-12 w-12 outline outline-1 outline-neutral-900 rounded-full" src="../../images/logos/defaultAvatar.wireframe.svg" alt="Default avatar" />
-}
